33632fbb2e5245e68386100f2b9313650ff2c708
0xjund <155731315+0xjund@users.noreply.github.com>
2024-03-05 10:56:46 +0900

    Lesson 3- Intro to Solidity
    Lesson 3- Intro to Solidity

---
+++ Simple Storage/ExtraStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8; 
+
+
+import "./SimpleStorage.sol";
+
+contract ExtraStorage is SimpleStorage{
+    //+5
+    //override 
+    //virutal override
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber +5;
+    }
+}
